cc4d8b6f075861956c416ca2d9b2707ea4b11e7d
Md Nurul Islam <98460077+NurulIslam17@users.noreply.github.com>
2022-06-19 11:59:30 +0600

    Expression and Template Literals

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDom.render(<h1>React Starting Code</h1>,document.getElementById('root'));
+const name = "Nurul";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    <div>
+      <h1>Expression and Template Literals</h1>
+      <h3>My name is {name}</h3>
+      <p>My lucky digit {1 + 2}</p>
+      <h4>Template Literals</h4>
+      <p>{`Name is ${name} and age is ${20+4}`}</p>
+    </div>
+
+    <div>
+      <h1>React Starting Code</h1>
+      <h2>Md. Nurul Islam</h2>
+      <ol>
+        <li>C/C++</li>
+        <li>Java/Javascript</li>
+        <li>PHP/Laravel</li>
+        <li>Reactjs</li>
+      </ol>
+    </div>
+  </>
+);
