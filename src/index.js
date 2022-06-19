286071ee8aef58623bce0c2c0ea79367091ff66c
Md Nurul Islam <98460077+NurulIslam17@users.noreply.github.com>
2022-06-19 12:09:31 +0600

    Display Current Date and Time in JSX

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const name = "Nurul";
+const date = new Date().toLocaleDateString();
+const time = new Date().toLocaleTimeString();
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
     <div>
+      <h2>Display Current Date and Time in JSX</h2>
+       <h3>{  `Today : ${date}` }</h3>
+       <h3>{  `Time : ${time}` }</h3>
+    </div>
+
+    <div>
       <h1>Expression and Template Literals</h1>
       <h3>My name is {name}</h3>
       <p>My lucky digit {1 + 2}</p>
       <h4>Template Literals</h4>
-      <p>{`Name is ${name} and age is ${20+4}`}</p>
+      <p>{`Name is ${name} and age is ${20 + 4}`}</p>
     </div>
 
     <div>
