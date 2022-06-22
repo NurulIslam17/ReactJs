d26e6465c44f2384ff3ca35bb0eee60c3110730b
Md Nurul Islam <98460077+NurulIslam17@users.noreply.github.com>
2022-06-22 22:58:38 +0600

    Adding meterails icon and dependencies

--- src/components/IncreDecre/IncreDecre.jsx
+++ src/components/IncreDecre/IncreDecre.jsx
@@ -1,33 +1,38 @@
-import React, { useState } from 'react'
-import './IncreDecre.css';
+import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import Button from '@material-ui/core/Button';
+import "./IncreDecre.css";
 
 export default function IncreDecre() {
-
-  let n=0;
-  let [num,setNum] = useState(n);
-  const increment = () =>{
+  let n = 0;
+  let [num, setNum] = useState(n);
+  const increment = () => {
     setNum(num + 1);
-  }
-  const decrement = () =>{
-    if(num>0){
-      setNum(num-1);
+  };
+  const decrement = () => {
+    if (num > 0) {
+      setNum(num - 1);
+    } else {
+      setNum(0);
+      alert("Negative Value Avoided. Increase the value");
     }
-    else{
-      setNum(0)
-      alert('Negative Value Avoided');
-    }
-    
-  }
+  };
+
+  // default
   return (
-    <div className='main btn'>
-      <div className='Display'>
+    <div className="main btn">
+      <div className="Display">
         <h1> {num} </h1>
-        <div className='btn' >
-          <button onClick={increment} className='inc'> Increment </button>
-          <button onClick={decrement} className='dec'> Decrement </button>
+        <div className="btn">
+          <Button onClick={increment} color="secondary">
+            <AddIcon/>
+          </Button>
+          <Button onClick={decrement} color="primary">
+            <RemoveIcon/>
+          </Button>
         </div>
       </div>
-      
     </div>
-  )
+  );
 }
