e05ad5618aa527df3ba98da8034bd5a2c95b8db7
Md Nurul Islam <98460077+NurulIslam17@users.noreply.github.com>
2022-06-24 19:52:02 +0600

    React API Call to Get Pokemon JSON Data using Axios and useEffect

--- src/components/Hook/SelectBox.jsx
+++ src/components/Hook/SelectBox.jsx
@@ -1,23 +1,51 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./SelectBox.css";
+import axios from "axios";
 
 function SelectBox() {
-  const [num,setNum] = useState(0);
+  const [num, setNum] = useState(0);
+  const [name, setName] = useState();
+  const [move, setMoves] = useState();
+
+  useEffect(() => {
+    async function getData() {
+      const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
+      console.log(res);
+      setName(res.data.name);
+      setMoves(res.data.moves.length);
+    }
+    getData();
+  });
   return (
     <>
       <div className="main">
         <div className="res">
-          <h1>Select Box value is {num}</h1>
+          <h3>
+            You have Choosen <span style={{ color: "red" }}>{num} value</span>{}
+          </h3>
+          <h3>
+            I am<span style={{ color: "red" }}> {name}</span>
+          </h3>
+          <h3>
+            I have <span style={{ color: "red" }}>{move} moves</span>
+          </h3>
 
-          <select className="select" value={num} onChange={(e)=>{
-            setNum(e.target.value);
-          }}>
+          <select
+            className="select"
+            value={num}
+            onChange={(e) => {
+              setNum(e.target.value);
+            }}
+          >
             <option value="1">Select Value</option>
             <option value="12">12</option>
-            <option value="35">35</option>
+            <option value="35">25</option>
             <option value="4">4</option>
-            <option value="1">1</option>
+            <option value="423">423</option>
+            <option value="323">323</option>
+            <option value="33">33</option>
+            <option value="112">112</option>
           </select>
         </div>
       </div>
