490390c73f866ae5c5a530cd9b60ee39b6105f4a
Md Nurul Islam <98460077+NurulIslam17@users.noreply.github.com>
2022-06-24 22:30:11 +0600

    Select Box

--- src/components/Hook/SelectBox.jsx
+++ src/components/Hook/SelectBox.jsx
@@ -21,32 +21,34 @@ function SelectBox() {
     <>
       <div className="main">
         <div className="res">
-          <h3>
-            You have Choosen <span style={{ color: "red" }}>{num} value</span>{}
-          </h3>
-          <h3>
-            I am<span style={{ color: "red" }}> {name}</span>
-          </h3>
-          <h3>
-            I have <span style={{ color: "red" }}>{move} moves</span>
-          </h3>
-
-          <select
-            className="select"
-            value={num}
-            onChange={(e) => {
-              setNum(e.target.value);
-            }}
-          >
-            <option value="1">Select Value</option>
-            <option value="12">12</option>
-            <option value="35">25</option>
-            <option value="4">4</option>
-            <option value="423">423</option>
-            <option value="323">323</option>
-            <option value="33">33</option>
-            <option value="112">112</option>
-          </select>
+          <div className="divSelect">
+            <h3>
+              You have Choosen <span style={{ color: "red" }}>{num} value</span>
+              {}
+            </h3>
+            <h3>
+              I am<span style={{ color: "red" }}> {name}</span>
+            </h3>
+            <h3>
+              I have <span style={{ color: "red" }}>{move} moves</span>
+            </h3>
+            <select
+              className="select"
+              value={num}
+              onChange={(e) => {
+                setNum(e.target.value);
+              }}
+            >
+              <option value="1">Select Value</option>
+              <option value="12">12</option>
+              <option value="35">25</option>
+              <option value="4">4</option>
+              <option value="423">423</option>
+              <option value="323">323</option>
+              <option value="33">33</option>
+              <option value="112">112</option>
+            </select>
+          </div>
         </div>
       </div>
     </>
